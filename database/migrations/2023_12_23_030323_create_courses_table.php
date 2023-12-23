4e13f96b221288908faecacb0100d6f48044f171
marsel945 <rewoherson519@gmail.com>
2023-12-23 11:11:33 +0800

    create migration course

--- database/migrations/2023_12_23_030323_create_courses_table.php
+++ database/migrations/2023_12_23_030323_create_courses_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('courses', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('title', 255);
+            $table->integer('duration')->comment('per month');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
