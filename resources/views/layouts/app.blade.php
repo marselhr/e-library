1f460f4aff7f872c88e5070bd8efde588585b064
marsel945 <rewoherson519@gmail.com>
2023-12-23 12:02:08 +0800

    create components

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> {{ $title ?? env('APP_NAME') }} </title>
+
+    <link rel="stylesheet" href="{{ asset('assets/compiled/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/compiled/css/app-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/compiled/css/iconly.css') }}">
+</head>
+
+<body>
+    <script src="{{ asset('assets/static/js/initTheme.js') }}"></script>
+    <div id="app">
+        <!-- Sidebar -->
+        @include('layouts.sidebar')
+
+        <div id="main" class='layout-navbar navbar-fixed'>
+
+            <!-- Navbar -->
+
+            @include('layouts.navbar')
+            <div id="main-content">
+
+                @yield('content')
+
+
+            </div>
+
+            @include('layouts.footer')
+        </div>
+    </div>
+    <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
+    <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+
+    <script src="{{ asset('assets/compiled/js/app.js') }}"></script>
+
+
+
+    <!-- Need: Apexcharts -->
+    <script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/static/js/pages/dashboard.js') }}"></script>
+
+</body>
+
+</html>
