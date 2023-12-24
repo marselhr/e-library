65c9ecfa04101b798d2577b63b977762cdd89894
marsel945 <rewoherson519@gmail.com>
2023-12-24 12:49:21 +0800

    count data dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\CourseMaterial;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('dashboard');
+        $courses = Course::all()->count();
+        $courseMaterials = CourseMaterial::all()->count();
+        return view('dashboard', compact('courses', 'courseMaterials'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                                 </div>
                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                     <h6 class="text-muted font-semibold">Total Course</h6>
-                                    <h6 class="font-extrabold mb-0">112.000</h6>
+                                    <h6 class="font-extrabold mb-0">{{ number_format($courses, 0, '.', '.') }}</h6>
                                 </div>
                             </div>
                         </div>
@@ -31,7 +31,7 @@
                                 </div>
                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                     <h6 class="text-muted font-semibold">Materials</h6>
-                                    <h6 class="font-extrabold mb-0">183.000</h6>
+                                    <h6 class="font-extrabold mb-0">{{ number_format($courseMaterials, 0, '.', '.') }}</h6>
                                 </div>
                             </div>
                         </div>
