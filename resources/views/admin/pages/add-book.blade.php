0120d16155cf62d3a0b2ff762bf19ac2026ae9b0
marsel945 <rewoherson519@gmail.com>
2023-12-30 22:03:04 +0800

    fix undifined routers

--- resources/views/admin/pages/add-book.blade.php
+++ resources/views/admin/pages/add-book.blade.php
@@ -10,7 +10,7 @@
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('courses.index') }}">Buku</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('book.index') }}">Buku</a></li>
                             <li class="breadcrumb-item active " aria-current="page">Tambah Buku
                             </li>
                         </ol>
