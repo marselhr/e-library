4df7a3b072bdcbdc3950808f0a153ee2b0f681f5
marsel945 <rewoherson519@gmail.com>
2024-01-03 00:14:35 +0800

    add facebook auth

--- config/services.php
+++ config/services.php
@@ -33,7 +33,12 @@ return [
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECREET'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => '/auth/google/callback'
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => '/auth/facebook/callback'
     ]
 ];
